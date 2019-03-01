7a885dbbd485236cc07f84d06236f15953090c71
Singgih Harseno <singgih.harseno12@gmail.com>
2019-03-01 16:44:55 +0700

    Add Timber and Constant

---
+++ imagepicker/src/main/java/com/esafirm/imagepicker/helper/Constants.java
@@ -0,0 +1,7 @@
+package com.esafirm.imagepicker.helper;
+
+public class Constants {
+    public static String CAMERA_FOLDER = "Camera";
+    public static String HI_APP_IMAGES_FOLDER = "HiAppImages";
+    public static String HI_APP_WALLPAPER_FOLDER = "HiAppWallpaper";
+}
